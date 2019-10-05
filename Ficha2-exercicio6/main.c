f81dd7ec9287b9d4d7895b452551a7d31502a81a
David Silvério <david.a.silverio@gmail.com>
2019-10-05 23:45:34 +0100

    Execise 6 done

---
+++ Ficha2-exercicio6/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { 
+	float nCarros, volFaturado, soma, carroVend, fatVenda, salBase = 600;
+
+	printf("Indique o numero de carros vendidos: ");
+	scanf("%f", &nCarros);
+	
+	printf("\nIndique o volume faturado: ");
+	scanf("%f", &volFaturado);
+
+	carroVend = nCarros * 100;
+	fatVenda = volFaturado * 0.015;
+	soma = salBase + fatVenda + carroVend;
+
+	printf("VENCIMENTO:\n");
+	printf("Salario Base:\t\t\t\t\t%.2f\n", salBase);
+	printf("Por Carro Vendido: \t\t\t\t%.2f\n", carroVend);
+	printf("1.50%% das Vendas Faturadas: \t\t\t%.2f", fatVenda);
+	printf("\n\t\t\t\t\t\t-------------\n");
+	printf("TOTAL:\t\t\t\t\t\t%.2f\n", soma);
+	
+	return 0;
+}
