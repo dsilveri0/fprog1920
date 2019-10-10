cb0d1b006f0dfd1ea42f5f4f7646ad6ddaed04c4
David Silvério Anunciação <2190730@my.ipleiria.pt>
2019-10-10 11:55:49 +0100

    changes on exe2 - assignment 3

--- Ficha3-exercicio2/main.c
+++ Ficha3-exercicio2/main.c
@@ -8,14 +8,17 @@ int main()
     printf("Introduza os 3 valores inteiros das linhas: ");
     scanf("%d%d%d", &linha1, &linha2, &linha3);
 
-    if((linha1 - linha2) < linha3 && linha3 < linha1 + linha2) {
-        if(linha1 == linha2 && linha2 == linha3) {
+    if(abs(linha1 - linha2) < linha3 && linha3 < linha1 + linha2) {
+
+        if(linha1 == linha2 && linha2 == linha3)
             printf("Triangulo Equilatero");
-        } else if(linha1 != linha2 && linha1 != linha3 && linha2 != linha3) {
+
+        else if(linha1 != linha2 && linha1 != linha3 && linha2 != linha3)
             printf("Triangulo Escaleno");
-        } else if(((linha1 == linha2) && linha1 != linha3) || ((linha1 == linha3) && linha1 != linha2) || ((linha2 == linha3) && linha2 != linha1)) {
+
+        else if(((linha1 == linha2) && linha1 != linha3) || ((linha1 == linha3) && linha1 != linha2) || ((linha2 == linha3) && linha2 != linha1))
             printf("Triangulo Isosceles");
-        }
+
     } else
         printf("Nao forma triangulo!");
 
