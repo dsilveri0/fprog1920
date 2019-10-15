db88284316fcf8b323d5084c1cfded5ed4dcf094
David Silverio <david.a.silverio@gmail.com>
2019-10-15 19:55:38 +0100

    Completed exercises 5, 6 and 7

---
+++ exe_casa/exe5/exe5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2;
+    float res;
+    char op;
+
+    printf("Numero 1: ");
+    scanf("%d", &num1);
+
+    printf("\nNumero 2: ");
+    scanf("%d", &num2);
+
+    printf("\nIntroduza uma operacao aritmetica: ");
+    scanf(" %c", &op);
+
+    switch (op) {
+    case '+':
+        res = num1 + num2;
+        printf("Resultado: %.0f", res);
+        break;
+    case '-':
+	res = num1 - num2;
+        printf("Subtracao: %.0f", res);
+        break;
+    case 'x':
+    case '*':
+	res = num1 * num2;
+        printf("Multiplicacao: %.0f", res);
+        break;
+    case ':':
+    case '/':
+	if(num1 != 0 && num2 != 0) {
+		res = num1 / num2;
+		printf("Divisao: %.1f", res);
+	} else
+		printf("Erro - Divisao por zero");
+	break;
+    case '%':
+	res = num1 % num2;
+        printf("Resto da divisao inteira: %0.f", res);
+        break;
+    default:
+        printf("Operador desconhecido!");
+    }
+
+    return 0;
+}
